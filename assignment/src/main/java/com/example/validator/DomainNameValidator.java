1967a099f3688e426f3678684cd354fcb280e0a9
wzq <1140459998@qq.com>
2021-11-26 21:09:21 +0800

    修改抛出异常提示

--- assignment/src/main/java/com/example/validator/DomainNameValidator.java
+++ assignment/src/main/java/com/example/validator/DomainNameValidator.java
@@ -10,26 +10,26 @@ public class DomainNameValidator {
 
     public void validateGetShortUrl(String longUrl) {
         if (Strings.isNullOrEmpty(longUrl)) {
-            throw new IllegalArgumentException("url is null or empty");
+            throw new IllegalArgumentException("url不能为null或者为空");
         }
         if (StringUtils.isEmpty(longUrl)){
-            throw new IllegalArgumentException("url is empty");
+            throw new IllegalArgumentException("url为空");
         }
         if (longUrl.length() > CommonConstants.MAX_SUPPORT_URL_LENGTH) {
-            throw new IllegalArgumentException("max support url length is: " + CommonConstants.MAX_SUPPORT_URL_LENGTH);
+            throw new IllegalArgumentException("url最大长度为: " + CommonConstants.MAX_SUPPORT_URL_LENGTH);
         }
     }
 
     public void validateGetLongUrl(String shortUrl) {
         if (Strings.isNullOrEmpty(shortUrl)) {
-            throw new IllegalArgumentException("url is null or empty");
+            throw new IllegalArgumentException("url为null或者为空");
         }
         String[] splitString = shortUrl.split("/");
         if (splitString.length != 4) {
-            throw new IllegalArgumentException("url is not illegal");
+            throw new IllegalArgumentException("短域名url不符合规定，请输入符合规定的url");
         }
         if (!shortUrl.contains(CommonConstants.BASE_URL)) {
-            throw new IllegalArgumentException("url is not illegal");
+            throw new IllegalArgumentException("url域名不符合规定");
         }
     }
 
